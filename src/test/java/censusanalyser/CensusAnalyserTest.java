9cd147d1a9782457a107c7a85d64e2f5eac313dc
UtpalMaji <utpalmaji.um@gmail.com>
2020-08-19 19:27:53 +0530

    Added: Test case to check file header, if wrong should throw Exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,6 +9,9 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_TYPE_PATH = "./src/test/resources/IndiaStateCensusData.txt";
+    private static final String INVALID_HEADER_TYPE_PATH = "./src/test/resources/InvalidHeaderIndiaStateCensusData.csv";
+
+
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -41,7 +44,19 @@ public class CensusAnalyserTest {
             exceptionRule.expect( CensusAnalyserException.class );
             censusAnalyser.loadIndiaCensusData( WRONG_CSV_TYPE_PATH );
         } catch ( CensusAnalyserException e ) {
-            Assert.assertEquals( CensusAnalyserException.ExceptionType.NOT_CSV_TYPE, e.type );
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.NOT_CSV_TYPE_OR_INVALID_HEADER, e.type );
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WhenInvalidHeader_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect( CensusAnalyserException.class );
+            censusAnalyser.loadIndiaCensusData( INVALID_HEADER_TYPE_PATH );
+        } catch ( CensusAnalyserException e ) {
+            Assert.assertEquals( CensusAnalyserException.ExceptionType.NOT_CSV_TYPE_OR_INVALID_HEADER, e.type );
         }
     }
 }
